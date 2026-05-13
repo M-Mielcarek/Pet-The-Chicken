26c08080025f6ab0cc93d90e614040466e9d8766
M-Mielcarek <malgorzata.m.mielcarek@gmail.com>
2026-05-13 11:00:31 +0200

    Add files via upload

---
+++ sklepikarz_dialogue.gd
@@ -0,0 +1,13 @@
+extends BaseDialogueTestScene
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+		var baloon = load("res://dialogue_baloon/balloon.tscn").instantiate()
+		get_tree().current_scene.add_child(baloon)
+		baloon.start(resource, title)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
